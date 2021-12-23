61cd3584b212df91ed9587830d4eaea16891b3ca
birgitmelissa <94128573+birgitmelissa@users.noreply.github.com>
2021-12-23 19:07:31 +0100

    Add files via upload

---
+++ opdracht21/script.js
@@ -0,0 +1,19 @@
+const makePizza = function(){
+	console.log("Make dough, ");
+	console.log("Turn dough into flat circle, ");
+	console.log("Spread tomato sause on top of dough, ");
+	console.log("Put vegetables and cheese on top of sause.");
+}
+
+const makeSushi = function(){
+	console.log("Cook rice, ");
+	console.log("Flat rice on seeweed, ");
+	console.log("Put fish and vegetables on rice, ");
+	console.log("Roll up seeweed.");
+}
+
+makeSushi()
+makeSushi()
+makeSushi()
+makePizza()
+makePizza()
